bc9f26adeeabee0a040456f5a7e3bc07ebb8e03b
Robin Littiere <72390360+robLittiere@users.noreply.github.com>
2022-12-01 10:39:37 +0100

    added Party and PartyComment models

---
+++ lib/models/Party.dart
@@ -0,0 +1,10 @@
+class Party{
+  String uid;
+  String owner;
+  String type;
+  DateTime eventDate;
+  List<String>participants;
+
+  Party(this.uid, this.owner, this.type, this.eventDate, this.participants);
+
+}
